4ad59c51811499a635301b2786bd54579af830cf
BanafshaHotak <azmeenashah@yahoo.com>
2020-06-14 18:36:39 +0500

    practice with java

---
+++ app.js
@@ -0,0 +1,339 @@
+// 1. Write a script to greet your website visitor using JS alert box.
+// alert("Hello World!");
+//2. Write a script to display Error please enter a valid password.
+// alert("Error! Please enter a valid password.");
+//3. Write a script to display Welcome to JS Land...<br>Happy Coding!
+// alert("Welcome to JS Land..." +"\n"+  "Happy Coding!");
+//4. Write messages in sequence.
+// alert("Welcome to JS Land");
+// alert("Happy coding!");
+
+//5. Generate messagge through browser's developer console.
+// console.log ("Hello... I can run JS through my web browser's concole");
+
+//6. Make use of alerts in HTML and CSS Project
+
+// Assignment #2
+// 1. Declare a variable called username
+// var username = "Mark";
+// alert(username);
+
+// 2. Declare a variable called myName & assign to it a string
+// 
+//3. Declaring variable, assigning message and running alert
+// var message = "Hello World";
+// alert(message);
+
+// 4.Script to save student’s bio data in JS variables
+// var Name = "Jhone Doe";
+// alert(Name);
+// var Age = "15 years old";
+// alert(Age);
+// var Course = "Certified Mobile Application Development";
+// alert(Course);
+
+// 5. Script using one JS variable
+// var Word = "PIZZA";
+// alert(Word +"\n"+ Word[0] + Word[1] + Word[2] + Word[3] +"\n"+ Word[0] + Word[1] + Word[2] +"\n"+ Word[0] + Word[1] +"\n"+ Word[0]);
+// 6. Declaring a variable and assigning email address
+// var email = "azmeenashah@yahoo.com";
+// alert("My email address is " + email);
+
+// 7. Declare a variable called book & assign value
+// var book = "A Smarter way To Learn JavaScript";
+// alert("I am trying to lean from the Book " + book);
+
+// 8. Write a script to display in browser through JS
+
+// document.write("Yah! I can write HTML Content through JavaScript");
+
+// 9. “▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬”“▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬”
+
+// var design = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+
+          //Assignment#3
+//1. Declare a variable called age & assign to it your age
+// var age = "I am 15 years old";
+// alert(age);
+//2. Declare & initialize a variable to Show “You have visited this site N times”.
+// var visit = "You have visited this site 14 times"; 
+// alert(visit);
+
+// 3. Declare a variable called birthYear & assign to it your birth year
+
+// var birthYear = "My bithyear is 1990";
+// alert(birthYear+"\n"+ "Data type of my ddeclared variable is number");
+
+
+// 4. Shopping from an online clothing store
+
+
+// var Name = "John Doe";
+// var product = "T-shirt(s)"
+// var quantity = "5";
+// document.write(Name +"<space> ordered <space>" + quantity + product + "<space> on XYZ Clothing store <space>");
+
+            // Assignment # 4
+
+// 1. Declare 3 variables in one statement
+// var x=5, y=10, z=20;
+// alert(x+y+z);                 
+
+
+// 2. Declare 5 legal & 5 illegal variable names.
+            // legal variables
+// var Name = "Saba";
+// var Course1 = "JavaScript";
+// var $moneyPaid = "500";
+//var _itemsPurchased = "20";
+//var Age = "20years";
+
+            // Illegal variables
+// var @email = "abc@xyz.com";
+// var 2cars = "HondaCivic";
+// var #count = "10";
+// var date of birth = "10 10 20";
+// var #2 = "done";
+
+// 3. Display Heading stating “Rules for naming JS variables”
+//a)
+// var heading = ("Rules for naming JS variables");
+//  document.write("<h3> Rules for naming JS variables </h3>");
+//b)
+// var num = "numbers";
+// var x = "$";
+// var alph = "letters";
+// var y = "_"; 
+// document.write("Variables names can only contain, " + " " + num +"," + " " + x + "," +" " + alph + " " +"and" + " " + y + "." +"For example: $my_1stVariable");
+//c)
+// document.write(" <br> Variables must begin with a, " + " " + alph +"," + " " + x + "," +" " + " " +"or" + " " + y + "." +"For example: $name, _name or name");
+//d)
+// document.write("<br>Variable names are case sensetive");
+//e)
+// document.write("<br>Variable names should not be JS keywords");
+  
+            //  Assignment#5
+//1. SUM of two numbers
+// var a = 3;
+// var b = 5;
+// var c = a+b;
+// document.write("Sum of"+" "+ a +" " +"and" + " " +b +" "+ "is"+" "+ c);
+//2. Repeat task1
+//              //Subtraction
+//  var a = 3;
+//  var b = 5;
+//  var c = a-b;
+// document.write("<br> Subtration of"+" "+ a +" " +"and" + " " +b +" "+ "is"+" "+ c);             
+//         //Multiplication
+// var a = 3;
+// var b = 5;
+// var c = a*b;
+// document.write("<br> Multiplication of"+" "+ a +" " +"and" + " " +b +" "+ "is"+" "+ c);
+
+//          //Modulas
+// var a = 3;
+// var b = 5;
+// var c = a/b;
+// document.write("<br> Modulus of"+" "+ a +" " +"and" + " " +b +" "+ "is"+" "+ c);
+
+//3.Do the following using JS Mathematic Expressions
+//a.
+// var x;
+
+//b.
+// x = "num";
+// document.write("value after declaration is num");
+
+//c.
+// num = 5;
+//d
+
+// document.write("<br>Initial value: 5"+".");
+//e
+// num++;
+//f
+// document.write("<br> Value after Incriment is: " + num); 
+//g
+
+// num = num+7;
+//h
+// document.write("<br> Value after addition is: " + num);
+//I
+// num--;
+//J
+// document.write("<br> Value after decrement is: " + num);
+//k
+// num = num/3;
+// document.write("<br> Value after divison is: " + num);
+//L
+
+// document.write("<br> The remainder is:0");
+         
+// 4.
+// var Price = 600; 
+// var amount = 5;
+// totPrice = Price*amount;
+// document.write("Total cost to buy" +" "+ amount +" "+ "tickets to a movie is" +" "+ totPrice +"PKR");
+
+// 6. The Temperature Converter
+// var C;
+// var F = 77;
+// C = (F-32)*5/9
+// C = (77-32)*5/9
+// C = 25
+// document.write(C+" " +"C"+" is"+ F); 
+
+// var F;
+// var C = 21.11111111111
+// F = (C*9/5)+32
+// F = ( 21.11111111111*9/5)+32
+// F = 70
+// document.write("<br>" +F +" " +"F"+" is"+ C);
+
+//7. 
+// var item1 = 650;
+// var Q_item1= 3;
+// var item2 = 100;
+// var Q_item2 = 7;
+// var S_charges = 100;
+// var Total = 2750;
+
+// document.write("Price of item1 is" +" "+item1);
+// document.write("<br> Quantity of item1 is" +" "+Q_item1);
+// document.write("<br> Price of item2 is" +" "+item2);
+// document.write("<br> Quantity of item2 is" +" "+Q_item2);
+// document.write("<br>Shipping Charges"+" "+ S_charges);
+// document.write("<br> Total cost of your order is"+" "+ Total);
+
+//8.
+// var Total_marks = 980;
+// var Obtain_marks = 804;
+// percentage = Obtain_marks/ Obtain_marks *100;
+// percentage = 809/980*100;
+// percentage = 82.04081632;
+// document.write("Total Marks"+" "+Total_marks);
+// document.write("<br>Marks Obtained"+" "+Obtain_marks);
+// document.write("<br>Percentage"+" "+percentage+" "+ "%");
+
+//9.
+// var dollar = 10;
+// var Riyal = 25;
+// var total_currency = 10*104.80+25*28;
+// var total_currency = 1748;
+// document.write("Total Currency in PKR is"+" "+total_currency);
+
+//10.
+// var num = 11;
+// num = num+5;
+// alert(num);
+// num = num*10;
+// alert(num);
+// num = num/2;
+// alert(num);
+
+//11.
+// var c_Yr = 2016;
+// var b_Yr = 1992;
+// var age;
+// age = c_Yr-b_Yr;
+// age = 2016-1992;
+// age = 24;
+// document.write("<h3> Age Calculator </h3>");
+// document.write("<br>Current Year:"+" "+c_Yr);
+// document.write("<br>Birth Year:"+" "+c_Yr);
+// document.write("<br>Your age is:"+" "+age);
+
+//12. a.
+// var r = 20;
+//b.
+// var c = 2*3.142*20;
+// var c = 125.68;
+// var A = 3.142*400;
+// var A = 1256.8;
+
+// document.write("<h3> The Geomtrizer </h3>");
+// document.write("<br>Radius of a Circle is:"+" "+r);
+// document.write("<br>The Circumference is:"+" "+c);
+// document.write("<br>The areais:"+" "+A);
+
+//13.
+// var snack = "Chocolate chip";
+// var c_age = 60;
+// var max_age = 65;
+// var amount_day = 3;
+// var total = c_age*amount_day;
+// var total = 65700;
+
+// document.write("<h3> The Life Time Supply Calculator </h3>");
+// document.write("<br>Favourit Snack:"+" "+snack);
+// document.write("<br>Current Age:"+" "+c_age);
+// document.write("<br>Estimated Age:"+" "+max_age);
+// document.write("<br>Amount of snacks per day:"+" "+amount_day);
+// document.write("<br>You will need"+" "+total+" "+snack+" "+"to last you until the ripe old age of"+" "+ max_age);
+
+            // Assignment 6-9
+
+// var a = 10;
+// document.write("The value of a is :"+" "+a);
+// ++a;
+// document.write("<br><br> The value of ++a is :"+" "+a);
+// document.write("<br> Now the value of a is :"+" "+a);
+
+// a++;
+// document.write("<br><br> The value of a++ is :"+" "+"11");
+// document.write("<br> Now the value of a is :"+" "+a);
+
+// --a;
+// document.write("<br><br> The value of --a is :"+" "+a);
+// document.write("<br> Now the value of a is :"+" "+a);
+
+// a--;
+// document.write("<br><br> The value of a-- is :"+" "+" "+ "11");
+// document.write("<br> Now the value of a is :"+" "+a);
+
+//2.
+// var a = 2;
+// document.write("a is"+" "+ a);
+// var b = 1;
+// document.write("<br>b is"+" "+ b);
+// var result = --a - --b + ++b + b--; 
+// document.write("<br>result is"+" "+ result);
+
+//Result explanation should check with comments applied on it to make sure the result being display
+// --a; a=1;
+// --a - --b; 1-0
+// --a - --b + ++b; 1-0+1
+// --a - --b + ++b +b-- 1-0+1+1=3; 
+
+//3.
+
+// var quest= "Enter Your Name";
+// var Ans =  "Welcome";
+
+// var quest = prompt(quest,Ans);
+
+            // Assignment 9-10
+
+            //1.
+// var city = prompt("enter your city");
+
+// if (city=="Karachi"){
+//   alert("welcome to the city of lights");
+// }
+// else{
+//   alert("city name is wrong");
+// }
+
+//2.
+
+// var city = prompt("enter your Gender");
+
+// if (Gender=="Female"){
+//   alert("Welcome maam")
+// }
+// else if(Gender=="Male"){
+//   alert("Welcome Sir")
+// }
+
+
